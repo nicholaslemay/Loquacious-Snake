b15e6863e16ec7feb8a95344ad714091bd8ab73b
Nicholas Lemay <nlemay@pyxis-tech.co>
2010-03-25 23:03:57 -0400

    Removed now useless files.

--- src/FluentSelenium/helpers/TestMethodDiscoveryHelper.py
+++
@@ -1,8 +0,0 @@
-class TestMethodDiscoveryHelper:
-    @staticmethod
-    def GetTestMethods(className, prefix):
-        methodNames=[]
-        for methodName in dir(className):
-            if str(methodName).startswith(prefix):
-                methodNames.append(methodName)
-        return methodNames
