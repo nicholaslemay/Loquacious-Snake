74ce9fbfa7660fe3dd3d3d5c7c98a9a00bc212de
Nicholas Lemay <nlemay@pyxis-tech.co>
2010-04-02 17:40:41 -0400

    Added __repr__ and __str__.

--- src/FluentSelenium/SeleniumDrivenUser.py
+++ src/FluentSelenium/SeleniumDrivenUser.py
@@ -11,11 +11,23 @@ class SeleniumDrivenUser:
         self.actions = SeleniumDrivenUserActions(seleniumExecutionContext)
         self.expectations = SeleniumDrivenUserExpectations(seleniumExecutionContext)
 
+        self.actions.chainingElement = self
+        self.expectations.chainingElement = self
+        
     def __getattr__(self, name):
         if hasattr(self.actions, name):
             return getattr(self.actions, name)
             
         elif hasattr(self.expectations, name):
             return getattr(self.expectations, name)
-        else:
+        try:
+            return getattr(self, name)
+        except Exception:
             raise UnknownMethodException("SeleniumDrivenUser does not support the " + name +  " method")
+    
+
+    def __repr__(self):
+        return str(self)
+    
+    def __str__(self):
+        return "SeleniumDrivenUser instance"
